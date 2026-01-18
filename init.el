03b246181d8dad50d1e63e38581d9259e6d2492a
vbuser <vbuser@vbuser>
2026-01-18 11:09:41 -0800

    Add init.el for VM

--- init.el
+++ init.el
@@ -1,14 +1,16 @@
 ;;; init.el --- Emacs initialization file
 ;;; Commentary:
-;; Load configuration from config.org
+;; Load Elpaca setup first, then config.org
 
 ;;; Code:
+
+;; Load Elpaca package manager from scripts directory
+(load (expand-file-name "scripts/elpaca-setup.el" user-emacs-directory))
+
+;; Now load config.org
 (org-babel-load-file
  (expand-file-name
   "config.org"
   user-emacs-directory))
 
 ;;; init.el ends here
-INITEL
-
-echo "✓ Created init.el"
